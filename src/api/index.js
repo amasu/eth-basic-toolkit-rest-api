e484483654a09fc8d8582c285aefd0b6a9e2d115
oussama.achhab <oussama.achhab@gmail.com>
2018-03-03 13:51:38 +0100

    completed

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,12 @@ import facets from './facets';
 export default ({ config, db }) => {
 	let api = Router();
 
+	const ethers = require('ethers');
+	const Wallet = require('ethers').Wallet;
+	const providers = ethers.providers;
+
+	const provider = providers.getDefaultProvider('rinkeby');
+
 	// mount the facets resource
 	api.use('/facets', facets({ config, db }));
 
@@ -15,25 +21,46 @@ export default ({ config, db }) => {
 
 	//GET /createWallet
 	api.get('/createWallet', (req, res)=>{
-		res.json({ message: 'Here will be created the new wallet' });
+		
+		const wallet = Wallet.createRandom();
+		
+		let pk = wallet.privateKey;
+		let addr = wallet.address;
+		console.log('PK : '+ pk);
+		console.log('Address : ' + addr);
+		res.json({privateKey : pk, address : addr});
 	});
 
 	//GET /getBalance/:param
 	api.get('/getBalance/:addr', (req, res)=>{
 		const addr = req.params.addr;
-		res.json({message:`Returns the balance of the address: ${addr}`});
+	
+		provider.getBalance(addr)
+			.then((balance)=>{
+				let ethBalance = ethers.utils.formatEther(balance);
+				res.json({ balance : ethBalance});
+				console.log("Balance : "+ ethBalance);
+			});
+
+		
 	});
 
-	//POST /transaction {privateKey, destination, amount} 
+	//POST /transaction {privateKey, destination, amount} in the body as x-www-form-urlencoded !!
 	api.post('/transaction',(req,res)=>{
 			
-			let privateKey = req.body.privateKey;
-			let destination = req.body.destination;
-			let amount = req.body.amount;
-			console.log(req.body);
-			res.send(privateKey + ' ' + destination + ' ' + amount);
-			
-			//res.sendStatus(201);
+			const privateKey = req.body.privateKey;
+			const destination = req.body.destination;
+			const amount = ethers.utils.parseEther(req.body.amount);
+
+			const wallet  = new ethers.Wallet(privateKey);
+			wallet.provider = provider;
+
+			const transactionPromise = wallet.send(destination, amount);
+
+			transactionPromise.then((transactionHash)=>{
+				res.send(transactionHash);
+			});
+
 		});
 
 	return api;
