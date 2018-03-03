1ccbc4c36364b2f0f277a1471c785f9afadbfba2
oussama.achhab <oussama.achhab@gmail.com>
2018-03-03 11:41:49 +0100

    routes created

--- src/api/index.js
+++ src/api/index.js
@@ -13,5 +13,28 @@ export default ({ config, db }) => {
 		res.json({ version });
 	});
 
+	//GET /createWallet
+	api.get('/createWallet', (req, res)=>{
+		res.json({ message: 'Here will be created the new wallet' });
+	});
+
+	//GET /getBalance/:param
+	api.get('/getBalance/:addr', (req, res)=>{
+		const addr = req.params.addr;
+		res.json({message:`Returns the balance of the address: ${addr}`});
+	});
+
+	//POST /transaction {privateKey, destination, amount} 
+	api.post('/transaction',(req,res)=>{
+			
+			let privateKey = req.body.privateKey;
+			let destination = req.body.destination;
+			let amount = req.body.amount;
+			console.log(req.body);
+			res.send(privateKey + ' ' + destination + ' ' + amount);
+			
+			//res.sendStatus(201);
+		});
+
 	return api;
 }
--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.use(bodyParser.json({
 	limit : config.bodyLimit
 }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // connect to db
 initializeDb( db => {
 
